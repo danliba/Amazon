56d7c1954463e3b6f2a45d30ccc16c7103675e24
Daniel Lizarbe <b383184@l40057.lvt.dkrz.de>
2025-08-25 17:44:47 +0200

    5 day released fixed

---
+++ Atlantic/notebooks/Atlantic_parcels_5.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --job-name=Atlantic_parcels_2
+#SBATCH --ntasks=23
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=23
+#SBATCH --mem=250G
+#SBATCH --exclusive
+#SBATCH --time=08:00:00
+#SBATCH --partition=compute
+#SBATCH --account=bk1450
+
+module load python3/2023.01-gcc-11.2.0
+
+# Activate your environment if needed
+source /sw/spack-levante/mambaforge-22.9.0-2-Linux-x86_64-kptncg/etc/profile.d/conda.sh
+conda activate /work/bk1450/b383184/conda/envs/parcels_3.1.2
+
+# run 185 day batches on each of the eight tasks
+num_particles=10000
+run_time_days=185
+ref_date="2022-01-01"
+
+for offset in $(seq -w 155 50 1255); do
+    # Use decimal to avoid octal with leading zeros
+    offset_end=$((10#$offset + 45))
+
+    srun --export=ALL --ntasks 1 --nodes 1 --exclusive -c 32 /bin/bash -c -l '
+      ref_date="'"$ref_date"'"
+      num_particles="'"$num_particles"'"
+      run_time_days="'"$run_time_days"'"
+      offset_str="'"$offset"'"
+      offset_end='"$offset_end"'
+
+      # Generate decimal day numbers (no -w) to avoid octal issues, pass as strings later
+      seq '"$((10#$offset))"' 5 "$offset_end" | xargs -n1 -P5 -I{} \
+        papermill Atlantic_parcels_4.ipynb \
+          executed/Atlantic_parcels_4.y'"${ref_date}"'-offset-days{}.ipynb \
+          -k python \
+          -p offset {} \
+          -p ref_date "$ref_date" \
+          -p num_particles "$num_particles" \
+          -p run_time_days "$run_time_days"
+    ' &
+done
+wait
+
+# executed/Atlantic_parcels_4.y${ref_date}-offset-days{}.ipynb
+# executed/Atlantic_parcels_4.y\${ref_date}.offset-days{}.ipynb \
---
+++ Atlantic/notebooks/Atlantic_parcels_5days_ver2.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#SBATCH --job-name=Atlantic_parcels_2
+#SBATCH --ntasks=23
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=23
+#SBATCH --mem=250G
+#SBATCH --exclusive
+#SBATCH --time=08:00:00
+#SBATCH --partition=compute
+#SBATCH --account=bk1450
+
+module load python3/2023.01-gcc-11.2.0
+
+# Activate your environment if needed
+source /sw/spack-levante/mambaforge-22.9.0-2-Linux-x86_64-kptncg/etc/profile.d/conda.sh
+conda activate /work/bk1450/b383184/conda/envs/parcels_3.1.2
+
+# run 185 day batches on each of the eight tasks
+num_particles=10000
+run_time_days=185
+
+# Split 2022 into two chunks
+year=2022
+min_doy=155
+max_doy=200
+srun --export=ALL --ntasks 1 --nodes 1 --exclusive -c 32 /bin/bash -c -l "seq -w $min_doy 5 $max_doy | xargs -n1 -P5 -I{} papermill Atlantic_parcels_3.ipynb executed/Atlantic_parcels_3.y${year}-doy{}.ipynb -k python -p start_year $year -p start_day_of_year {} -p num_particles $num_particles -p run_time_days $run_time_days" & 
+
+year=2022
+min_doy=205
+max_doy=250
+srun --export=ALL --ntasks 1 --nodes 1 --exclusive -c 32 /bin/bash -c -l "seq -w $min_doy 5 $max_doy | xargs -n1 -P5 -I{} papermill Atlantic_parcels_3.ipynb executed/Atlantic_parcels_3.y${year}-doy{}.ipynb -k python -p start_year $year -p start_day_of_year {} -p num_particles $num_particles -p run_time_days $run_time_days" & 
+
+
+year=2022
+min_doy=255
+max_doy=300
+srun --export=ALL --ntasks 1 --nodes 1 --exclusive -c 32 /bin/bash -c -l "seq -w $min_doy 5 $max_doy | xargs -n1 -P5 -I{} papermill Atlantic_parcels_3.ipynb executed/Atlantic_parcels_3.y${year}-doy{}.ipynb -k python -p start_year $year -p start_day_of_year {} -p num_particles $num_particles -p run_time_days $run_time_days" & 
+
+year=2022
+min_doy=305
+max_doy=365
+srun --export=ALL --ntasks 1 --nodes 1 --exclusive -c 32 /bin/bash -c -l "seq -w $min_doy 5 $max_doy | xargs -n1 -P5 -I{} papermill Atlantic_parcels_3.ipynb executed/Atlantic_parcels_3.y${year}-doy{}.ipynb -k python -p start_year $year -p start_day_of_year {} -p num_particles $num_particles -p run_time_days $run_time_days" & 
+
+
+# Split 2023 into two chunks
+year=2023
+min_doy=0
+max_doy=180
+srun --export=ALL --ntasks 1 --nodes 1 --exclusive -c 32 /bin/bash -c -l "seq -w $min_doy 5 $max_doy | xargs -n1 -P5 -I{} papermill Atlantic_parcels_3.ipynb executed/Atlantic_parcels_3.y${year}-doy{}.ipynb -k python -p start_year $year -p start_day_of_year {} -p num_particles $num_particles -p run_time_days $run_time_days" & 
+
+year=2023
+min_doy=185
+max_doy=365
+srun --export=ALL --ntasks 1 --nodes 1 --exclusive -c 32 /bin/bash -c -l "seq -w $min_doy 5 $max_doy | xargs -n1 -P5 -I{} papermill Atlantic_parcels_3.ipynb executed/Atlantic_parcels_3.y${year}-doy{}.ipynb -k python -p start_year $year -p start_day_of_year {} -p num_particles $num_particles -p run_time_days $run_time_days" & 
+
+# Split 2024 into two chunks
+year=2024
+min_doy=0
+max_doy=180
+srun --export=ALL --ntasks 1 --nodes 1 --exclusive -c 32 /bin/bash -c -l "seq -w $min_doy 5 $max_doy | xargs -n1 -P5 -I{} papermill Atlantic_parcels_3.ipynb executed/Atlantic_parcels_3.y${year}-doy{}.ipynb -k python -p start_year $year -p start_day_of_year {} -p num_particles $num_particles -p run_time_days $run_time_days" & 
+
+year=2024
+min_doy=185
+max_doy=365
+srun --export=ALL --ntasks 1 --nodes 1 --exclusive -c 32 /bin/bash -c -l "seq -w $min_doy 5 $max_doy | xargs -n1 -P5 -I{} papermill Atlantic_parcels_3.ipynb executed/Atlantic_parcels_3.y${year}-doy{}.ipynb -k python -p start_year $year -p start_day_of_year {} -p num_particles $num_particles -p run_time_days $run_time_days" & 
+
+# Split 2025 into two chunks
+year=2025
+min_doy=0
+max_doy=75
+srun --export=ALL --ntasks 1 --nodes 1 --exclusive -c 32 /bin/bash -c -l "seq -w $min_doy 5 $max_doy | xargs -n1 -P5 -I{} papermill Atlantic_parcels_3.ipynb executed/Atlantic_parcels_3.y${year}-doy{}.ipynb -k python -p start_year $year -p start_day_of_year {} -p num_particles $num_particles -p run_time_days $run_time_days" & 
+
+year=2025
+min_doy=80
+max_doy=155
+srun --export=ALL --ntasks 1 --nodes 1 --exclusive -c 32 /bin/bash -c -l "seq -w $min_doy 5 $max_doy | xargs -n1 -P5 -I{} papermill Atlantic_parcels_3.ipynb executed/Atlantic_parcels_3.y${year}-doy{}.ipynb -k python -p start_year $year -p start_day_of_year {} -p num_particles $num_particles -p run_time_days $run_time_days" & 
+
+wait
