d01f7e300d2b82e7c747abc820a5f4207ee69a58
Daniel Lizarbe <b383184@l40076.lvt.dkrz.de>
2026-02-10 17:27:48 +0100

    Update OSM

---
+++ Mercator/notebooks/Analysis/Season.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=Season
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --mem=250G
+#SBATCH --exclusive
+#SBATCH --time=08:00:00
+#SBATCH --partition=compute
+#SBATCH --account=bk1450
+
+module load python3/2023.01-gcc-11.2.0
+
+source /sw/spack-levante/mambaforge-22.9.0-2-Linux-x86_64-kptncg/etc/profile.d/conda.sh
+conda activate /work/bk1450/b383184/conda/envs/parcels_3.1.2
+
+mkdir -p executed logs
+
+papermill Seasonality.ipynb executed/Seasonality_executed.ipynb -k python3
